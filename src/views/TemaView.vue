d89f0b9f7d11e7167ca9f6e2c8eb16eab7d3be26
Azizmurod <murodovazizmurod@gmail.com>
2023-08-15 00:10:02 +0500

    tema page :)

--- src/views/TemaView.vue
+++ src/views/TemaView.vue
@@ -3,13 +3,3 @@
         Here will be 'threads' section lol :)
     </div>
 </template>
-
-<script>
-    export default {
-        data() {
-            return {
-                id: this.$route.params.id
-            }
-        }
-    }
-</script>
